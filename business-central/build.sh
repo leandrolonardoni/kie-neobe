1aa75a962150c216ae561653971d1ea572ee90f6
Leandro Lonardoni <leandro.lonardoni@benner.com.br>
2019-05-22 10:29:05 -0300

    Inclusão dos arquivos no repositório

---
+++ business-central/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ***************************************************
+# jBPM Workbench Showcase - Docker image build script
+# ***************************************************
+
+IMAGE_NAME="jboss/jbpm-workbench-showcase"
+IMAGE_TAG="latest"
+
+
+# Build the container image.
+echo "Building the Docker container for $IMAGE_NAME:$IMAGE_TAG.."
+docker build --rm -t $IMAGE_NAME:$IMAGE_TAG .
+echo "Build done"
